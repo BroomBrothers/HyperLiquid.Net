cc3068f6a817ebb2341f202dca7df19b5ce78104
Carter <carter@broombrothers.org>
2026-01-29 21:24:27 +0200

    Add HyperLiquidTriggerCondition

--- HyperLiquid.Net/Objects/Models/HyperLiquidOrder.cs
+++ HyperLiquid.Net/Objects/Models/HyperLiquidOrder.cs
@@ -2,6 +2,7 @@ using CryptoExchange.Net.Converters.SystemTextJson;
 using HyperLiquid.Net.Enums;
 using System;
 using System.Text.Json.Serialization;
+using HyperLiquid.Net.Converters;
 
 namespace HyperLiquid.Net.Objects.Models
 {
@@ -90,7 +91,8 @@ namespace HyperLiquid.Net.Objects.Models
         /// Trigger condition
         /// </summary>
         [JsonPropertyName("triggerCondition")]
-        public string TriggerCondition { get; set; } = string.Empty;
+        [JsonConverter(typeof(HyperLiquidTriggerConditionConverter))]
+        public HyperLiquidTriggerCondition? TriggerCondition { get; set; }
         /// <summary>
         /// Trigger price
         /// </summary>
